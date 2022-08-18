c34d382c3e1a003090ed65ecade74cdf3dfddab1
hasaan <hassaankhan5550@gmail.com>
2022-08-18 06:37:54 +0500

    added practice.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Large from "./components/Large";
 import Medium from "./components/Medium";
 import Navbar from "./components/Navbar";
+import Practice from "./components/Practice";
 import Small from "./components/Small";
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <div className="App">
       <h1 className="text-success">hy</h1>
       <Navbar/>
+
+      <Practice/>
 
       <Small/>
 
---
+++ src/components/Practice.js
@@ -0,0 +1,9 @@
+const Practice = () => {
+    return ( 
+        <div id="Main_Practice">
+            <h1>Lets Check First</h1>
+        </div>
+     );
+}
+ 
+export default Practice;
