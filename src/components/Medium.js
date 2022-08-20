06945266a7716874375e9d2fea0475ab7ed27bd4
Muhammad Hassaan Mahmood <hassaankhan5550@gmail.com>
2022-08-20 17:01:35 +0500

    this day

--- src/components/Medium.js
+++ src/components/Medium.js
@@ -1,4 +1,37 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Medium = () => {
+
+    //--start = get window, screen, pixels, width, height
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+          width,
+          height
+        };
+      }
+  
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    return windowDimensions;
+  }
+
+const { height, width } = useWindowDimensions();
+ console.log(" height", height, "width", width );
+
+ //--end = get window, screen, pixels, width, height
+
     return ( 
         <div id="Main_Medium">
             <h1>Medium Codes</h1>
