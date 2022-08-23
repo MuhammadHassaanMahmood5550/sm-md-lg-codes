8d15fda13bff84a6db2c061e3d33d777021d07e3
hasaan <hassaankhan5550@gmail.com>
2022-08-23 20:33:05 +0500

    just removed commits

--- src/components/FirstTry.js
+++ src/components/FirstTry.js
@@ -1,11 +1,5 @@
 const FirstTry = () => {
  
-
-
-    // condition1 ? value1
-    //     : condition2 ? value2
-    //     : condition3 ? value3
-    //     : value4;
     return ( 
         <div id="Main_FirstTry">
             <h1>Lets Check First</h1>
