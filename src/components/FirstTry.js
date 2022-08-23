16be0502f14861e9b719d5456673204eaa833881
hasaan <hassaankhan5550@gmail.com>
2022-08-23 20:31:29 +0500

    change practice.js

---
+++ src/components/FirstTry.js
@@ -0,0 +1,16 @@
+const FirstTry = () => {
+ 
+
+
+    // condition1 ? value1
+    //     : condition2 ? value2
+    //     : condition3 ? value3
+    //     : value4;
+    return ( 
+        <div id="Main_FirstTry">
+            <h1>Lets Check First</h1>
+        </div>
+     );
+}
+ 
+export default FirstTry;
