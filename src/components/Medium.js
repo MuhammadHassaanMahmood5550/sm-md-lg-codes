8b98b8102961f8632cf49e7ab1fa084c1872f1e9
hasaan <hassaankhan5550@gmail.com>
2022-08-28 16:02:44 +0500

    added useMemo

--- src/components/Medium.js
+++ src/components/Medium.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useLayoutEffect } from "react";
+import { useEffect, useState, useRef, useLayoutEffect, useMemo } from "react";
 import ButtonAsChild from "./ButtonAsChild";
 const Medium = () => {
 
@@ -10,7 +10,6 @@ const Medium = () => {
 //           height
 //         };
 //       }
-
 
 // function useWindowDimensions() {
 //     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
@@ -61,8 +60,6 @@ const Medium = () => {
       // setUp("h");
       //************** useLayoutEffect = useLayoutEffect called before useEffect end
 
-
-
 //   ***** we can do same word with props like passing state and function but some time it is hard to pass at top level so, useImperitive hook
 // useImperativeHandle = allow you to use state or function of child componenet   into the parent componenet. here parent componenet means parent component 
 //   using another(child component) in it.  ******
@@ -74,6 +71,61 @@ const Medium = () => {
 //  *****useImperativeHandle = allow you to use state or function of child componenet 
 // into the parent componenet. here parent componenet means parent component 
 // using another(child component) in it.  end *****
+
+
+//**********devide array into chunks of your choise */
+// const PER_BUCKET = 4
+// const items = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// console.log("items.length =-=", items.length);
+// // the following reducer splits the original array into "buckets" (arrays) of 8 items.
+// const newItems = items.reduce((result, item, i) => { 
+//  //   console.log("result =-=", result, "item=-=", item, "i=-=", i);
+//   const bucketIndex = Math.floor(i / PER_BUCKET);
+// //   console.log("bucketIndex =-=", bucketIndex);
+//   if (!result[bucketIndex]) {
+//     result[bucketIndex] = [];
+//   }
+//   result[bucketIndex].push(item);
+//   return result;
+// }, []);
+// console.log("final newItems =-=", newItems);
+// console.log("final newItems.length =-=", newItems.length);
+//**********devide array into chunks of your choise */
+
+//********promise = if you do this then we will do this */
+// let myPromise1 = new Promise((resolve, reject) => {
+//     let a = 1 + 1;
+//     if(a === 2) {
+//         resolve("success");  
+//     }else{
+//         reject("failed");
+//     }
+// })
+
+// myPromise1.then((message) => {
+//     console.log("we are in then message = ", message);
+// }).catch((message) => {
+//     console.log("we are in catch message = ", message);
+// });
+//********promise = if you do this then we will do this */
+
+//******** remember when state change in our application that components rerenders
+//// and that is why we use useMemo to control this.
+// const [state1, setState1] = useState(0);
+// const [state2, setState2] = useState(0);
+
+// const checkFubction = () => {
+// console.log('====================================');
+// console.log("this function run when state1 changes =//= ", state1 + 1);
+// console.log('====================================');
+// }
+////now useMemo know that only run function when [state1](dependency) change and not other states on the components.
+//// checkFubctionMemo = even dont need to call but can cal any where
+// const checkFubctionMemo = useMemo(() => checkFubction(), [state1]); 
+//******** remember when state change in our application that components rerenders
+//// and that is why we use useMemo to control this          end heres
+
 
 
     return ( 
@@ -104,6 +156,20 @@ const Medium = () => {
     *  useImperativeHandle = allow you to use state or function of child componenet 
   into the parent componenet. here parent componenet means parent component 
   using another(child component) in it.  end  *****/}
+
+
+  {/* //******** remember when state change in our application that components rerenders
+// and that is why we use useMemo to control this. */}
+            {/* {checkFubctionMemo} */}
+            {/* <button
+            onClick={() => setState2(state2 + 1)}
+            >Change state2</button>
+            <br />
+            <button
+            onClick={() => setState1(state1 + 1)}
+            >Change state1</button> */}
+{/* //******** remember when state change in our application that components rerenders
+// and that is why we use useMemo to control this. */}
 
 
         </div>
